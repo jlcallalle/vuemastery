a4726329c68dba1ed1ca57ca85cf05e81d4efd82
Jorge Callalle <jlcallalle@gmail.com>
2021-09-14 00:02:27 -0500

    vuex state

--- Mastering vuex/real-world-vue-lesson11-vuex-start/src/views/EventCreate.vue
+++ Mastering vuex/real-world-vue-lesson11-vuex-start/src/views/EventCreate.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create Event - {{ $store.state.user.name }}</h1>
+    <p>This event was created by  {{ userName }} with id: {{ userId }}</p>
+  </div>
 </template>
+
+<script>
+export default {
+  computed: {
+    userName() {
+      return this.$store.state.user.name
+    },
+    userId() {
+      return this.$store.state.user.id
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
